780d1415e0bcc53f115043f94fca8f1a48b545e2
Jose Henrique <jh4832184@gmail.com>
2024-09-06 17:30:03 -0300

    view: add modal component

--- frontend_consorcio_automovel/src/pages/vehicles.jsx
+++ frontend_consorcio_automovel/src/pages/vehicles.jsx
@@ -2,6 +2,8 @@ import { Header } from "../components/header";
 import { Main } from "../components/main";
 import { Footer } from "../components/footer";
 import { Layout } from "../components/layout";
+import { Modal } from "../components/modal";
+import { Input } from "../components/input";
 
 export function Vehicles() {
   const array = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -9,14 +11,28 @@ export function Vehicles() {
     <Layout>
       <Header />
       <Main>
-        <div className="p-4 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-screen-xl mx-auto">
-          {array.map(num => (
-            <div key={num} className="mx-auto border p-4 bg-red-100 max-w-64">
-              <h1>Veículos</h1>
-              <p>Aqui você encontra todos os veículos cadastrados.</p>
-            </div>
-          ))}
-        </div>
+        <section>
+          <div className="flex justify-between items-center ">
+            <h1 className="text-lg font-semibold text-primary">Veiculos</h1>
+            <Modal title="Adicionar veículo">
+              <Input type="text" placeholder="Chassi" />
+              <Input type="text" placeholder="Modelo" />
+              <Input type="text" placeholder="Nome" />
+              <Input type="number" placeholder="Preço" />
+              <Input type="text" placeholder="Cor" />
+            </Modal>
+          </div>
+          
+          <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-screen-2xl mx-auto">
+            {array.map(num => (
+              <div key={num} className="mx-auto border p-4 bg-tertiary w-full">
+                <h1>Veículos</h1>
+                <p>Aqui você encontra todos os veículos cadastrados.</p>
+              </div>
+            ))}
+          </div>
+
+        </section>
       </Main>
       <Footer />
     </Layout>
