f9feb3163c20b776d1bf83a75872ae6f0e1bc3bc
Jose Henrique <jh4832184@gmail.com>
2024-09-07 18:42:44 -0300

    refactor: add for and disable props

--- frontend_consorcio_automovel/src/components/input.jsx
+++ frontend_consorcio_automovel/src/components/input.jsx
@@ -1,10 +1,12 @@
-export function Input({type, placeholder, value}) {
+export function Input({type, placeholder, value, disabled, id}) {
   return (
     <input
+      id={id}
       type={type}
       placeholder={placeholder}
       value={value? value: ""}
       className="border rounded px-2 py-1 hover:border-slate-300 focus:outline-none focus:border-primary"
+      disabled={disabled && true}
       required
       />
   );
