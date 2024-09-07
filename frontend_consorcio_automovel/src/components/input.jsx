7071f035818cb30cb6eced5b68a234d41144f61d
Jose Henrique <jh4832184@gmail.com>
2024-09-06 23:00:39 -0300

    fix: fix prop value input

--- frontend_consorcio_automovel/src/components/input.jsx
+++ frontend_consorcio_automovel/src/components/input.jsx
@@ -1,9 +1,11 @@
-export function Input({type, placeholder}) {
+export function Input({type, placeholder, value}) {
   return (
     <input
       type={type}
       placeholder={placeholder}
+      value={value? value: ""}
       className="border rounded px-2 py-1 hover:border-slate-300 focus:outline-none focus:border-primary"
-      required />
+      required
+      />
   );
 }
