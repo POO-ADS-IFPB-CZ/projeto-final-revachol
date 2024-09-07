6faebdd2a476be8bdd3b3cd998f728deaa55d06d
Jose Henrique <jh4832184@gmail.com>
2024-09-06 22:59:52 -0300

    view: add vehicle card

--- frontend_consorcio_automovel/src/pages/vehicles.jsx
+++ frontend_consorcio_automovel/src/pages/vehicles.jsx
@@ -4,6 +4,7 @@ import { Footer } from "../components/footer";
 import { Layout } from "../components/layout";
 import { Modal } from "../components/modal";
 import { Input } from "../components/input";
+import VehicleCard from "../components/vehicle-card";
 
 export function Vehicles() {
   const array = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -20,15 +21,13 @@ export function Vehicles() {
               <Input type="text" placeholder="Nome" />
               <Input type="number" placeholder="Preço" />
               <Input type="text" placeholder="Cor" />
+              <input type="file" className="bg-primary" />
             </Modal>
           </div>
           
           <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-screen-2xl mx-auto">
             {array.map(num => (
-              <div key={num} className="mx-auto border p-4 bg-tertiary w-full">
-                <h1>Veículos</h1>
-                <p>Aqui você encontra todos os veículos cadastrados.</p>
-              </div>
+              <VehicleCard key={num}/>
             ))}
           </div>
 
