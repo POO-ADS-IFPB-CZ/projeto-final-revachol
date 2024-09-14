ea38ede3101f28a33468e910e8152f12994ce237
IslanP1 <islanpereiradeoliveira@gmail.com>
2024-09-13 21:54:17 -0300

    feat: view vendedor

--- backend_consorcio_automovel/vendedor/views.py
+++ backend_consorcio_automovel/vendedor/views.py
@@ -1,12 +1,39 @@
+from django.http import HttpRequest
 from ninja import NinjaAPI
+from ninja.errors import HttpError
+from .controllers import VendedorController
+from .schemas import VendedorSchema, LoginSchema
 
 class VendedorView:
     def __init__(self):
-        self.api = NinjaAPI(version="1.1.0")
+        self.api = NinjaAPI(version="vendedor_v1")
         self.register_routes()
 
     def register_routes(self):
-        pass
+
+        @self.api.post("/cadastrar")
+        def cadastrar_vendedor(request: HttpRequest, data: VendedorSchema):
+            if not request.user.is_staff:
+                raise HttpError(403, "Entre como administrador para cadastrar um vendedor")
+            
+            VendedorController.criar_vendedor(data)
+            return {"sucess": "Vendedor cadastrado com sucesso"}
+    
+        @self.api.post("/login")
+        def login_vendedor(request: HttpRequest, data: LoginSchema):
+            user = VendedorController.login_vendedor(request, data)
+            if user is not None:
+                return {"success": "User autenticado"}
+            else:
+                raise HttpError(401, "Unauthorized")
+            
+        @self.api.post("/logout")
+        def logout_vendedor(request: HttpRequest):
+            if not request.user.is_authenticated:
+                raise HttpError(401, "User não autenticado")
+            else:
+                VendedorController.logout_vendedor(request)
+                return {"message": "Logout realizado com sucesso"}
         
 vendedor_view = VendedorView()
 api = vendedor_view.api
